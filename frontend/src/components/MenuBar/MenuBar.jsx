1ccd2963624e0710f67063abdadcbc46293920e0
N-Viveharan <nagalingamviveharan2001@gmail.com>
2026-04-08 19:45:45 +0530

    menubar

--- frontend/src/components/MenuBar/MenuBar.jsx
+++ frontend/src/components/MenuBar/MenuBar.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useTheme } from '../../context/ThemeContext';
-import styles from './MenuBar.module.css';
+import styles from './MenuBar.css';
 
 export default function MenuBar() {
   const { theme, toggle } = useTheme();
